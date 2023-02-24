3fabcf33cdefd7c90a9480a169021f78c6cf52f8
Danii Akhiyan <daniil.axiyan.316@mail.ru>
2023-02-24 21:27:04 +0300

    feat: add answer

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const encodeArr = expr.match(/.{1,10}/g);
+    let morseArr = new Array();
+    let decodeMorse = '';
+
+    encodeArr.forEach(val => {
+        let valArr;
+        val != "**********" ? valArr = val.match(/.{1,2}/g) : valArr = [val];
+        for (const iterator of valArr) {
+            if (iterator == '10') {
+                morseArr.push('.');
+            }else if(iterator == '11'){
+                morseArr.push('-');
+            }else if(iterator == '**********'){
+                morseArr.push('space');
+            }
+        }
+        morseArr.push(' ');
+
+    });
+    morseArr = morseArr.join('').split(' ');
+    for (const i of morseArr) {
+        if(Object.hasOwn(MORSE_TABLE, i)){
+            decodeMorse = decodeMorse + MORSE_TABLE[i];
+        }else if(i == 'space'){
+            decodeMorse = decodeMorse + ' ';
+        }
+        
+    }
+
+    return decodeMorse;
 }
 
 module.exports = {
